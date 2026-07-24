6624847f69b8d99b4f731c69f1440b47c12385f4
pk1762012 <pratik1762012@gmail.com>
2026-07-25 00:27:46 +0530

    build(android): target API 36 and exclude legacy IAP

--- react-native.config.js
+++ react-native.config.js
@@ -3,6 +3,15 @@ module.exports = {
       ios: {},
       android: {},
     },
+    dependencies: {
+      // IAP is used only by the iOS payment flow. Do not package its legacy
+      // Google Play Billing dependency in Android until Android IAP exists.
+      'react-native-iap': {
+        platforms: {
+          android: null,
+        },
+      },
+    },
     assets: ['./src/assets/fonts',
       
     ], 
